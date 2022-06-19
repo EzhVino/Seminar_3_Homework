f6fd00941b52a0cb09170929410083a79370f5ff
Ezh+Wine <valvic90@gmail.com>
2022-06-19 13:47:44 +0300

    task 2 completed

--- Program.cs
+++ Program.cs
@@ -44,29 +44,3 @@ else
 // int c2 = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("Расстояние между точками: " + Dots3Ddistance(a1, a2, b1, b2, c1, c2));
 
-//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
-
-// Console.Clear();
-
-// void ListSqrt(int number){
-//     int count = 1;
-
-//     Console.Write(number + " -> ");
-
-//     if(number>0){
-
-//     while (count<=number){
-//         Console.Write(Math.Pow(count, 3) + ", ");
-//         count++;
-//     }}
-//     else{
-//         count = -1;
-//         while (count>=number){
-//         Console.Write(Math.Pow(count, 3) + ", ");
-//         count--;
-//     }
-// }}
-
-// Console.WriteLine("Введите число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// ListSqrt(num);
